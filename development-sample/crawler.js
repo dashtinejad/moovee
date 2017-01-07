b15880faded169a281c14e6c19183313189bd8e9
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-01-07 11:17:31 +0330

    sample crawler

--- development-sample/crawler.js
+++ development-sample/crawler.js
@@ -1,22 +1,54 @@
+var Datastore = require('nedb')
 var Crawler = require("crawler")
 var url = require('url')
 
-// console.log(Crawler)
-// console.log(url)
+
+var db = new Datastore({
+  filename: './sample-nedb-db',
+  autoload: true
+})
 
 var c = new Crawler();
 
-c.queue({
-    uri: 'http://www.imdb.com/title/tt1345836/',
+db.findOne({ _id: 'tt1345836' }, (err, movie) => {
+    // console.log(movie._crawled)
 
-    // The global callback won't be called
-    callback: function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            console.log($("title").text());
+    c.queue({
+        // uri: 'http://www.imdb.com/title/tt1345836/',
+        html: movie._crawled,
+
+        // The global callback won't be called
+        callback: function (error, res, done) {
+            if(error){
+                console.log(error);
+            }else{
+                var $ = res.$;
+                const title = $('h1[itemprop="name"]')
+                    .clone()
+                    .find('#titleYear')
+                        .remove()
+                        .end()
+                    .text()
+                
+                const year = $('#titleYear').text().slice(1, -1)
+
+                const duration = $('[itemprop="duration"]').attr('datetime')
+
+                const genres = []
+                $('.subtext [itemprop="genre"]').each((index, item) => {
+                    genres.push($(item).text())
+                })
+
+                const date = $('meta[itemprop="datePublished"]').attr('content')
+
+                console.log(title)
+                console.log(year)
+                console.log(duration)
+                console.log(genres)
+                console.log(date)
+
+            }
+            done();
         }
-        done();
-    }
-});
+    });
+})
--- development-sample/nedb.js
+++ development-sample/nedb.js
@@ -1,31 +1,55 @@
 var Datastore = require('nedb')
 
+var Crawler = require("crawler")
+var url = require('url')
+
+// console.log(Crawler)
+// console.log(url)
+
 var db = new Datastore({
   filename: './sample-nedb-db',
   autoload: true
 })
 
-const movie = {
-  _id: 'tt1345836',
-  title: 'Dark Knight Rises',
-  year: 2012,
-}
+var c = new Crawler();
 
-// remove the old document
-/*
-db.remove(
-  { _id: 'tt1345836' },
-  { multi: true },
-  (err, numRemoved) => {
+c.queue({
+    uri: 'http://www.imdb.com/title/tt1345836/',
 
-  }
-)
+    // The global callback won't be called
+    callback: function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            
+            const movie = {
+              _id: 'tt1345836',
+              _crawled: res.body,
+              // title: 'Dark Knight Rises',
+              // year: 2012,
+            }
 
-db.insert(movie, (err, newDoc) => {
-  console.log(newDoc._id)
-})
-*/
+            // remove the old document
+            db.remove(
+              { _id: 'tt1345836' },
+              { multi: true },
+              (err, numRemoved) => {
 
-db.find({}, (err, docs) => {
-  console.log(docs)
-})
+              }
+            )
+
+            db.insert(movie, (err, newDoc) => {
+              console.log(newDoc._id)
+            })
+
+
+            /*
+            db.find({}, (err, docs) => {
+              console.log(docs)
+            })
+            */
+        }
+        done();
+    }
+});
